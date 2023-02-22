ea8ba7e23f87752893fd9abea91cf65bfca12ac0
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2023-02-22 10:54:36 +0530

    initial commit

--- src/main/java/com/example/Rest/RestApplication.java
+++ src/main/java/com/example/Rest/RestApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApplication {
 
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println("EVEN");
+			} else {
+				System.out.println("OUT");
+
+			}
+		}
+
 		SpringApplication.run(RestApplication.class, args);
 	}
 
